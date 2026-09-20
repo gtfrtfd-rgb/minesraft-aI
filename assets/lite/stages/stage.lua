90f8a8cf4cf19256a486f89fa046562d450fc376
gtfrtfd-rgb <gtfrtfd@gmail.com>
2026-09-20 18:56:07 +0700

    aeases

--- assets/lite/stages/stage.lua
+++
@@ -1,8 +0,0 @@
-local dir = "bg/stage/"
-
-function onCreate()
-    makeLuaSprite("stage", "bg/stage/stage", -1460, -1200)
-    scaleObject("stage", 2.2, 2.2)
-    setProperty("stage.antialiasing", false)
-    addLuaSprite("stage")
-end
